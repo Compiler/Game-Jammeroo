020d0cab1419c3491d42f6c883600a1775195a82
Compiler <lukerochedev@gmail.com>
2020-07-26 18:31:38 -0400

    itnerface for collision syetem

---
+++ Jammer/Jammer/src/Collision/CollisionSystem.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <SFML/Graphics/RectangleShape.hpp>
+
+
+namespace jam {
+
+
+	namespace CollisionSystem {
+
+
+		static bool aabbOverlapCheck(sf::Rect<float> r1, sf::Rect<float> r2) {
+
+
+			return false;
+		}
+
+
+		static bool isColliding(sf::Rect<float> r1, sf::Rect<float> r2) {
+
+			return false;
+		}
+
+
+	}
+
+}
