b5abd087ee34ee006e66723ed4ca7d1ab9d35bae
Compiler <lukerochedev@gmail.com>
2020-07-26 18:06:00 -0400

    thing

--- Jammer/Jammer/src/Entities/Entity.cpp
+++ Jammer/Jammer/src/Entities/Entity.cpp
@@ -1,5 +1,6 @@
 #include "Entity.h"
 namespace jam {
-//	Entity::Entity(const sf::Vector2f& p, const sf::Rect<float>& cb) : m_pos(p), m_collision_box(cb) {};
+	Entity::Entity(const sf::Vector2f& p, const sf::Rect<float>& cb) : m_pos(p), m_collision_box(cb) {};
+	Entity() {};
 //	Entity::Entity() {};
 }
