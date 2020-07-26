e5ee253c5e8adf873230850d2650ea97b7a0650c
KingPEPSALT <quinmackfoster+github@gmail.com>
2020-07-26 23:10:49 +0100

    this is messy but it has been fixed, youre welcome.

--- Jammer/Jammer/src/Entities/Entity.h
+++ Jammer/Jammer/src/Entities/Entity.h
@@ -7,6 +7,7 @@ namespace jam {
 		sf::Rect<float> m_collision_box;
 		sf::Vector2f m_pos;
 		Entity(const sf::Vector2f& p, const sf::Rect<float>& cb);
+		Entity() = default;
 	public:
 
 		inline bool isColliding(const sf::Rect<float>& other) const {return m_collision_box.intersects(other);}
