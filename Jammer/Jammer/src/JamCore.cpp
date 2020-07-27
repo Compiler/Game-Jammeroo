08484852c401be893406cd55ac20c5141e02b93a
Compiler <lukerochedev@gmail.com>
2020-07-26 22:06:03 -0400

    input keys registration

--- Jammer/Jammer/src/JamCore.cpp
+++ Jammer/Jammer/src/JamCore.cpp
@@ -3,6 +3,7 @@
 
 namespace jam {
 
+	void populateEvents(sf::Event event);
 
 	float JamCore::deltaTime;
 
@@ -50,6 +51,7 @@ namespace jam {
 		while (_window->pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 				_window->close();
+			populateEvents(event);
 			if (event.type == sf::Event::KeyReleased) {
 				if (event.key.code == sf::Keyboard::E) {
 					flip = !flip;
@@ -101,4 +103,79 @@ namespace jam {
 	}
 
 
+
+
+
+
+
+
+
+
+
+	void populateEvents(sf::Event event) {
+		if (event.type == sf::Event::KeyPressed) {
+			if (event.key.code == sf::Keyboard::Up) InputManager::addKeyPress(jam::KeyCodes::KEY_UP);
+			if (event.key.code == sf::Keyboard::Down) InputManager::addKeyPress(jam::KeyCodes::KEY_DOWN);
+			if (event.key.code == sf::Keyboard::Left) InputManager::addKeyPress(jam::KeyCodes::KEY_LEFT);
+			if (event.key.code == sf::Keyboard::Right) InputManager::addKeyPress(jam::KeyCodes::KEY_RIGHT);
+			if (event.key.code == sf::Keyboard::A) InputManager::addKeyPress(jam::KeyCodes::KEY_A);
+			if (event.key.code == sf::Keyboard::B) InputManager::addKeyPress(jam::KeyCodes::KEY_B);
+			if (event.key.code == sf::Keyboard::C) InputManager::addKeyPress(jam::KeyCodes::KEY_C);
+			if (event.key.code == sf::Keyboard::D) InputManager::addKeyPress(jam::KeyCodes::KEY_D);
+			if (event.key.code == sf::Keyboard::E) InputManager::addKeyPress(jam::KeyCodes::KEY_E);
+			if (event.key.code == sf::Keyboard::F) InputManager::addKeyPress(jam::KeyCodes::KEY_F);
+			if (event.key.code == sf::Keyboard::G) InputManager::addKeyPress(jam::KeyCodes::KEY_G);
+			if (event.key.code == sf::Keyboard::H) InputManager::addKeyPress(jam::KeyCodes::KEY_H);
+			if (event.key.code == sf::Keyboard::I) InputManager::addKeyPress(jam::KeyCodes::KEY_I);
+			if (event.key.code == sf::Keyboard::J) InputManager::addKeyPress(jam::KeyCodes::KEY_J);
+			if (event.key.code == sf::Keyboard::K) InputManager::addKeyPress(jam::KeyCodes::KEY_K);
+			if (event.key.code == sf::Keyboard::L) InputManager::addKeyPress(jam::KeyCodes::KEY_L);
+			if (event.key.code == sf::Keyboard::M) InputManager::addKeyPress(jam::KeyCodes::KEY_M);
+			if (event.key.code == sf::Keyboard::N) InputManager::addKeyPress(jam::KeyCodes::KEY_N);
+			if (event.key.code == sf::Keyboard::O) InputManager::addKeyPress(jam::KeyCodes::KEY_O);
+			if (event.key.code == sf::Keyboard::P) InputManager::addKeyPress(jam::KeyCodes::KEY_P);
+			if (event.key.code == sf::Keyboard::Q) InputManager::addKeyPress(jam::KeyCodes::KEY_Q);
+			if (event.key.code == sf::Keyboard::R) InputManager::addKeyPress(jam::KeyCodes::KEY_R);
+			if (event.key.code == sf::Keyboard::S) InputManager::addKeyPress(jam::KeyCodes::KEY_S);
+			if (event.key.code == sf::Keyboard::T) InputManager::addKeyPress(jam::KeyCodes::KEY_T);
+			if (event.key.code == sf::Keyboard::U) InputManager::addKeyPress(jam::KeyCodes::KEY_U);
+			if (event.key.code == sf::Keyboard::V) InputManager::addKeyPress(jam::KeyCodes::KEY_V);
+			if (event.key.code == sf::Keyboard::W) InputManager::addKeyPress(jam::KeyCodes::KEY_W);
+			if (event.key.code == sf::Keyboard::X) InputManager::addKeyPress(jam::KeyCodes::KEY_X);
+			if (event.key.code == sf::Keyboard::Y) InputManager::addKeyPress(jam::KeyCodes::KEY_Y);
+			if (event.key.code == sf::Keyboard::Z) InputManager::addKeyPress(jam::KeyCodes::KEY_Z);
+		}
+		if (event.type == sf::Event::KeyReleased) {
+			if (event.key.code == sf::Keyboard::Up) InputManager::addKeyRelease(jam::KeyCodes::KEY_UP);
+			if (event.key.code == sf::Keyboard::Down) InputManager::addKeyRelease(jam::KeyCodes::KEY_DOWN);
+			if (event.key.code == sf::Keyboard::Left) InputManager::addKeyRelease(jam::KeyCodes::KEY_LEFT);
+			if (event.key.code == sf::Keyboard::Right) InputManager::addKeyRelease(jam::KeyCodes::KEY_RIGHT);
+			if (event.key.code == sf::Keyboard::A) InputManager::addKeyRelease(jam::KeyCodes::KEY_A);
+			if (event.key.code == sf::Keyboard::B) InputManager::addKeyRelease(jam::KeyCodes::KEY_B);
+			if (event.key.code == sf::Keyboard::C) InputManager::addKeyRelease(jam::KeyCodes::KEY_C);
+			if (event.key.code == sf::Keyboard::D) InputManager::addKeyRelease(jam::KeyCodes::KEY_D);
+			if (event.key.code == sf::Keyboard::E) InputManager::addKeyRelease(jam::KeyCodes::KEY_E);
+			if (event.key.code == sf::Keyboard::F) InputManager::addKeyRelease(jam::KeyCodes::KEY_F);
+			if (event.key.code == sf::Keyboard::G) InputManager::addKeyRelease(jam::KeyCodes::KEY_G);
+			if (event.key.code == sf::Keyboard::H) InputManager::addKeyRelease(jam::KeyCodes::KEY_H);
+			if (event.key.code == sf::Keyboard::I) InputManager::addKeyRelease(jam::KeyCodes::KEY_I);
+			if (event.key.code == sf::Keyboard::J) InputManager::addKeyRelease(jam::KeyCodes::KEY_J);
+			if (event.key.code == sf::Keyboard::K) InputManager::addKeyRelease(jam::KeyCodes::KEY_K);
+			if (event.key.code == sf::Keyboard::L) InputManager::addKeyRelease(jam::KeyCodes::KEY_L);
+			if (event.key.code == sf::Keyboard::M) InputManager::addKeyRelease(jam::KeyCodes::KEY_M);
+			if (event.key.code == sf::Keyboard::N) InputManager::addKeyRelease(jam::KeyCodes::KEY_N);
+			if (event.key.code == sf::Keyboard::O) InputManager::addKeyRelease(jam::KeyCodes::KEY_O);
+			if (event.key.code == sf::Keyboard::P) InputManager::addKeyRelease(jam::KeyCodes::KEY_P);
+			if (event.key.code == sf::Keyboard::Q) InputManager::addKeyRelease(jam::KeyCodes::KEY_Q);
+			if (event.key.code == sf::Keyboard::R) InputManager::addKeyRelease(jam::KeyCodes::KEY_R);
+			if (event.key.code == sf::Keyboard::S) InputManager::addKeyRelease(jam::KeyCodes::KEY_S);
+			if (event.key.code == sf::Keyboard::T) InputManager::addKeyRelease(jam::KeyCodes::KEY_T);
+			if (event.key.code == sf::Keyboard::U) InputManager::addKeyRelease(jam::KeyCodes::KEY_U);
+			if (event.key.code == sf::Keyboard::V) InputManager::addKeyRelease(jam::KeyCodes::KEY_V);
+			if (event.key.code == sf::Keyboard::W) InputManager::addKeyRelease(jam::KeyCodes::KEY_W);
+			if (event.key.code == sf::Keyboard::X) InputManager::addKeyRelease(jam::KeyCodes::KEY_X);
+			if (event.key.code == sf::Keyboard::Y) InputManager::addKeyRelease(jam::KeyCodes::KEY_Y);
+			if (event.key.code == sf::Keyboard::Z) InputManager::addKeyRelease(jam::KeyCodes::KEY_Z);
+		}
+	}
 }
