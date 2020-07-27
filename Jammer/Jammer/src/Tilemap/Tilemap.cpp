71454a0aa63cf232057c81a6e494adcce9b1d309
KingPEPSALT <quinmackfoster+github@gmail.com>
2020-07-27 21:09:05 +0100

    please let this work

---
+++ Jammer/Jammer/src/Tilemap/Tilemap.cpp
@@ -0,0 +1,77 @@
+#include "Tilemap.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <stdarg.h>
+
+namespace jam {
+
+	TileLayerData::TileLayerData(const std::string& name, const std::vector<int>& data, unsigned int w):_layer_name(name),_csvdata(data), _width(w){}
+
+	//debug purposes
+	std::string TileLayerData::print() const {
+		auto it = _csvdata.begin();
+		std::stringstream ss;
+		ss << this->getName() << "\n\n";
+		size_t i = 0;
+		while (it != _csvdata.end()) {
+			i++;
+			ss << (*it);
+			if (i % 20 == 0) ss << '\n';
+			it++;
+		}
+		ss << "\n\n\n\n\n WIDTH: " << _width << "\nHEIGHT: " << height();
+		return ss.str();
+	}
+
+	Tilemap::Tilemap(const std::vector<std::string>& filepaths) {
+		for (auto filepath : filepaths) this->load_layer(filepath);
+	}
+	
+	Tilemap::Tilemap(const std::string& filepath) {
+		this->load_layer(filepath);
+	}
+
+
+	TileLayerData Tilemap::getData(const std::string& layer_name) const {
+		for (auto data : this->_data_vector)
+			if (data.getName() == layer_name)
+				return data;
+		// error lol
+		return TileLayerData();
+	}
+
+	void Tilemap::load_layer(const std::string& filepath) {
+		std::ifstream filestream;
+		std::string file_name = (filepath.substr((filepath).find_last_of("/\\") + 1)).substr(0, filepath.find_last_of("."));
+
+		filestream.open(filepath);
+		if (!filestream) {
+			std::cout << "Tilemap was not opened correctly" << std::endl;
+			return;
+		}
+		
+		std::vector<int> csvdata;
+		std::string line; 
+		std::stringstream ss;
+		std::string substr;
+		unsigned int width = 0;
+
+		while (filestream >> line){
+			ss = std::stringstream(line);
+			width = 0;
+			while (ss.good()) {
+				width++;
+				std::getline(ss, substr, ',');
+				int i = std::stoi(substr);
+				csvdata.push_back(i);
+			}
+		}
+
+		this->_data_vector.push_back(TileLayerData(file_name, csvdata, width));
+
+	}
+
+
+}
---
+++ Jammer/Jammer/src/Tilemap/Tilemap.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "Tileset.h"
+namespace jam {
+
+	struct TileLayerData {
+	private:
+		std::string _layer_name;
+		std::vector<int> _csvdata;
+		unsigned int _width;
+	public:
+		TileLayerData() = default;
+		TileLayerData(const std::string& name, const std::vector<int>& data, unsigned int width);
+		inline bool isLoaded() const { return !_csvdata.empty(); };
+		inline int height() const { return _csvdata.size() / _width; };
+		inline std::string getName() const { return _layer_name; };
+		// debug purposes
+		std::string print() const;
+	};
+
+	class Tilemap {
+		std::vector<TileLayerData> _data_vector;
+		Tileset _tileset;
+	public:
+
+		Tilemap() = default;
+		Tilemap(const std::vector<std::string>& filepaths);
+		Tilemap(const std::string& filepath);
+
+		TileLayerData getData(const std::string& layer_name) const;
+		void load_layer(const std::string& filepath);
+	};
+
+}
